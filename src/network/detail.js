80dd226773178559c76e163690193ae2bdedfdad
changyanwei <1926174536@qq.com>
2021-01-23 15:49:04 +0800

    数据整合、商品基本信息和店铺信息的展示

--- src/network/detail.js
+++ src/network/detail.js
@@ -8,3 +8,29 @@ export function getDetail(id) {
     }
   })
 }
+
+//商品基本信息
+export class GoodsInfo {
+  constructor(itemInfo,columns,services){
+    this.title = itemInfo.title;
+    this.desc = itemInfo.desc;
+    this.newPrice = itemInfo.price;
+    this.oldPrice = itemInfo.oldPrice;
+    this.discount = itemInfo.discountDesc;
+    this.realPrice = itemInfo.lowNowPrice;
+    this.columns = columns;
+    this.services = services;
+  }
+}
+
+//店铺信息
+export class Shop{
+  constructor(shopInfo) {
+    this.logo = shopInfo.shopLogo;
+    this.name = shopInfo.name;
+    this.fans = shopInfo.cFans;
+    this.sells = shopInfo.cSells;
+    this.score =  shopInfo.score;
+    this.goodsCount = shopInfo.cGoods;
+  }
+}
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,43 +1,56 @@
 <template>
   <div>
-    <detail-nav-bar />
-    <detail-swiper :top-images="topImages" />
+    <detail-nav-bar/>
+    <detail-swiper :top-images="topImages"/>
+    <detail-base-info :goods="goods" />
+    <detail-shop-info :shop="shop" />
   </div>
 </template>
 
 <script>
   import DetailNavBar from "./detailComponents/DetailNavBar";
   import DetailSwiper from "./detailComponents/DetailSwiper";
+  import DetailBaseInfo from "./detailComponents/DetailBaseInfo";
+  import DetailShopInfo from "./detailComponents/DetailShopInfo";
 
-  import {getDetail} from "network/detail";
+  import {getDetail, GoodsInfo, Shop} from "network/detail";
 
   export default {
     name: "Detail",
     data() {
       return {
         id: null,
-        topImages: []
+        topImages: [],
+        goods: {},
+        shop: {}
       }
     },
     components: {
       DetailNavBar,
-      DetailSwiper
+      DetailSwiper,
+      DetailBaseInfo,
+      DetailShopInfo
     },
     created() {
       this.id = this.$route.params.id;
 
       this.getDetailData();
     },
-    activated() {
-
-    },
     methods: {
       //网络请求相关
       getDetailData() {
         getDetail(this.id).then(res => {
           console.log(res);
+
+          const data = res.result;
           //1.获取顶部轮播图数据
-          this.topImages = res.result.itemInfo.topImages;
+          this.topImages = data.itemInfo.topImages;
+
+          //2.获取商品信息
+          this.goods = new GoodsInfo(data.itemInfo, data.columns, data.shopInfo.services);
+
+          //3.获取店铺信息
+          this.shop = new Shop(data.shopInfo)
         })
       }
     }
