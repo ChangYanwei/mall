0e9424ac1b546ce257c7efc3e36db6a7713895cf
changyanwei <1926174536@qq.com>
2021-01-23 15:13:33 +0800

    详情页数据请求与轮播图展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,25 +1,45 @@
 <template>
   <div>
     <detail-nav-bar />
+    <detail-swiper :top-images="topImages" />
   </div>
 </template>
 
 <script>
-
   import DetailNavBar from "./detailComponents/DetailNavBar";
+  import DetailSwiper from "./detailComponents/DetailSwiper";
+
+  import {getDetail} from "network/detail";
 
   export default {
     name: "Detail",
     data() {
       return {
-        iid: null
+        id: null,
+        topImages: []
       }
     },
     components: {
-      DetailNavBar
+      DetailNavBar,
+      DetailSwiper
     },
     created() {
-      console.log(this.$route.params.id);
+      this.id = this.$route.params.id;
+
+      this.getDetailData();
+    },
+    activated() {
+
+    },
+    methods: {
+      //网络请求相关
+      getDetailData() {
+        getDetail(this.id).then(res => {
+          console.log(res);
+          //1.获取顶部轮播图数据
+          this.topImages = res.result.itemInfo.topImages;
+        })
+      }
     }
   }
 </script>
